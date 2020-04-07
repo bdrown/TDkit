b8f01c9a0c2ef533d738d529945c2077b3f479ab
Bryon Drown <bryon.drown@gmail.com>
2020-04-07 10:59:31 -0400

    Created PfRM project skeleton

---
+++ PfRMruler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PfRMruler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project will eventually quantify proteoform abundance using PRM data.");
+        }
+    }
+}
