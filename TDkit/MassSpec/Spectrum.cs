76d43e7175e31bc3fe871039f37186cb2990bc75
Bryon Drown <bryon.drown@gmail.com>
2020-05-02 11:25:06 -0400

    New objects for describing mass spec experiment

---
+++ TDkit/MassSpec/Spectrum.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDkit.MassSpec
+{
+    class Spectrum : IMzData
+    {
+        private readonly double[] mz;
+        private readonly double[] intensity;
+
+        /// <summary>
+        /// Simple constructor for Spectrum instance
+        /// </summary>
+        /// <param name="mz">Array of mz data</param>
+        /// <param name="intensity">Array of intensity data</param>
+        public Spectrum(double[] mz, double[] intensity, double rt = 0.0, int MSLevel = 1, double precursor = 0.0)
+        {
+            this.intensity = intensity;
+            this.mz = mz;
+            this.RetentionTime = rt;
+            this.MSLevel = MSLevel;
+            this.Precursor = precursor;
+        }
+
+        /// <summary>
+        /// Simple constructor for Spectrum instance
+        /// </summary>
+        /// <param name="mz">List of mz data</param>
+        /// <param name="intensity">List of intensity data</param>
+        public Spectrum(List<double> mz, List<double> intensity, double rt = 0.0, int MSLevel = 1, double precursor = 0.0)
+        {
+            this.intensity = intensity.ToArray();
+            this.mz = mz.ToArray();
+            this.RetentionTime = rt;
+            this.MSLevel = MSLevel;
+            this.Precursor = precursor;
+        }
+
+        /// <summary>
+        /// Time at which Spectrum was acquired in minutes
+        /// </summary>
+        public double RetentionTime { get; }
+
+        /// <summary>
+        /// Level of MS acquisition. MSLevel > 1 indicates fragment spectrum.
+        /// </summary>
+        public int MSLevel { get; }
+
+        /// <summary>
+        /// mz selection used to acquire spectrum
+        /// </summary>
+        public double Precursor { get; }
+
+        /// <summary>
+        /// First m/z data value
+        /// </summary>
+        public double FirstMz => mz[0];
+
+        /// <summary>
+        /// Last m/z data value
+        /// </summary>
+        public double LastMz => mz[mz.Length - 1];
+
+        /// <summary>
+        /// Number of data points in spectrum
+        /// </summary>
+        public int Length => mz.Length;
+
+        public double[] GetIntensity()
+        {
+            return intensity;
+        }
+
+        public double[] GetMz()
+        {
+            return mz;
+        }
+    }
+}
---
+++ TDkit/MassSpec/Transition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDkit.MassSpec
+{
+    class Transition
+    {
+        public int PrecursorCharge { get; }
+
+        public int FragmentCharge { get; }
+
+        public Proteoform Precursor { get; }
+
+        public Fragment Fragment { get; }
+
+        public Transition(Proteoform parent, Fragment frag, int parentCharge, int fragCharge)
+        {
+            this.Precursor = parent;
+            this.Fragment = frag;
+            this.PrecursorCharge = parentCharge;
+            this.FragmentCharge = fragCharge;
+        }
+    }
+}
